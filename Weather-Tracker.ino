5b470344d40f029ff1264cac54ba730231c71e87
Evil Electronics Klub <130092222+evil-electronics-klub@users.noreply.github.com>
2023-11-06 19:10:16 -0500

    Update Weather-Tracker.ino

--- Weather-Tracker.ino
+++ Weather-Tracker.ino
@@ -1,25 +1,39 @@
 #include <ArduinoJson.h>
 #include <WiFiNINA.h>
+#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 #define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 32
-
+#define SCREEN_HEIGHT 64
 #define OLED_RESET -1
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-char ssid[] = "Your_SSID";      // your network SSID (name)
-char pass[] = "Your_Password"; // your network password
-int status = WL_IDLE_STATUS;   // the WiFi radio's status
+char ssid[] = "Your SSID";         // Replace with your WiFi network name
+char pass[] = "Your Pasword";     // Replace with your WiFi network password
+int status = WL_IDLE_STATUS;      // WiFi status
+const char* host = "api.openweathermap.org";
+const char* apiKey = "Your API Key";
+String city = "Your Town,us";
+String units = "imperial";
+
+enum WeatherDisplayState {
+  TEMPERATURE,
+  HUMIDITY,
+  PRESSURE
+};
+
+WeatherDisplayState currentState = TEMPERATURE;
+unsigned long stateChangeTime = 0;
 
 void setup() {
   Serial.begin(9600);
 
   // Initialize the OLED display
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
+    for (;;);
   }
 
   // Connect to WiFi network
@@ -31,66 +45,92 @@ void setup() {
   }
 
   Serial.println("Connected to WiFi");
-
   display.clearDisplay();
   display.display();
 }
 
 void loop() {
-  // Make a request to OpenWeatherMap API
-  WiFiClient client;
-  const char* host = "api.openweathermap.org";
-  const char* apiKey = "Your_API_Key";
-  String url = "/data/2.5/weather?q=Patchogue,us&units=imperial&appid=" + String(apiKey); //note your city and units will be different
-
-  if (!client.connect(host, 80)) {
-    Serial.println("Connection failed");
-    return;
+  switch (currentState) {
+    case TEMPERATURE:
+      displayTemperature();
+      break;
+    case HUMIDITY:
+      displayHumidity();
+      break;
+    case PRESSURE:
+      displayPressure();
+      break;
   }
 
+  if (millis() - stateChangeTime >= 10000) {
+    // Switch to the next state after 10 seconds
+    switch (currentState) {
+      case TEMPERATURE:
+        currentState = HUMIDITY;
+        break;
+      case HUMIDITY:
+        currentState = PRESSURE;
+        break;
+      case PRESSURE:
+        currentState = TEMPERATURE;
+        break;
+    }
+    stateChangeTime = millis();
+  }
+}
+
+void displayTemperature() {
+  float temperature = getWeatherData("temp");
+  displayValue("Temp", temperature, "°F");
+}
+
+void displayHumidity() {
+  int humidity = getWeatherData("humidity");
+  displayValue("Humidity", humidity, "%");
+}
+
+void displayPressure() {
+  float pressure = getWeatherData("pressure");
+  displayValue("Pressure", pressure, "hPa");
+}
+
+float getWeatherData(const char* key) {
+  WiFiClient client;
+  String url = "/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=" + units;
+  
+  if (!client.connect(host, 80)) {
+    Serial.println("Connection failed");
+    return 0.0;
+  }
+  
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
                "Connection: close\r\n\r\n");
-
+  
   while (client.connected()) {
     String line = client.readStringUntil('\n');
     if (line == "\r") {
       break;
     }
   }
-
+  
   String response = client.readString();
   client.stop();
-
-  // Parse the response
-  DynamicJsonDocument doc(1024);
+  
+  DynamicJsonDocument doc(2048);
   deserializeJson(doc, response);
+  
+  JsonObject main = doc["main"];
+  return main[key];
+}
 
-  float temperature = doc["main"]["temp"];
-  float humidity = doc["main"]["humidity"];
-  float pressure = doc["main"]["pressure"];
-
-  // Display the values on the OLED screen
+void displayValue(const char* label, float value, const char* units) {
   display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.print("Temp: ");
-  display.print(temperature);
-  display.print("F");
-
-  display.setCursor(0,10);
-  display.print("Humidity: ");
-  display.print(humidity);
-  display.print("%");
-
-  display.setCursor(0,20);
-  display.print("Pressure: ");
-  display.print(pressure);
-  display.print("M");
-
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print(label);
+  display.setCursor(0, 20);
+  display.print(value);
   display.display();
-
-  // Wait for 10 seconds before making another request
-  delay(10000);
 }
